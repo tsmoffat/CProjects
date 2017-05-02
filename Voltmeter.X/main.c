40b156c236121b380f6f1fee651893051003e707
tomoffat <tsmoffat@gmail.com>
2017-05-02 11:24:11 +0100

    Main updated with hold function

--- Voltmeter.X/main.c
+++ Voltmeter.X/main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: Tom Moffat & James Pollard
  *
@@ -13,59 +13,92 @@
 
 #include <xc.h>
 #include <stdlib.h>
+#include <math.h>
 #include "lcd.h"
 #include "adc.h"
 unsigned int voltage = 0;
+unsigned int voltagemax = 0;
+unsigned int displaymode = 0;
+char voltagestring;
+eeprom int voltagesample;
 
-/*char conv(int a){
-    int bitvolt = a;
-    int bitvoltb = bitvolt;
-    bitvolt << 2;
-    bitvoltb >> 1;
-    bitvolt = bitvolt + bitvoltb;
-    bitvolt = bitvolt >> 5;
-    bitvolt = bitvolt >> 5;
-    float absolutev = bitvolt*4.5;
-    return absolutev;
-}*/
-
-void main(void) 
-{
+void main(void){
     TRISA = 0b00000011;
     TRISB = 0b01010000;//sets everything except RB0 as an output
     RW = 0;
     CS = 1;
     CLK = 0;
     Lcd_Init();
-    
-    Lcd_Clear(); 
-    Lcd_Set_Cursor(1,1);
-    Lcd_Write_String((char *)"Welcome");
-    __delay_ms(1000);
+
     Lcd_Clear();
     Lcd_Set_Cursor(1,1);
-    Lcd_Write_String((char *)"Range:");
-    __delay_ms(1000);
+    Lcd_Write_String((char *)"Welcome ");
+    __delay_ms(1500);
     Lcd_Clear();
     Lcd_Set_Cursor(1,1);
-    Lcd_Write_String((char *)"-0.6V->");
-    __delay_ms(1000);
-    Lcd_Clear();
-    Lcd_Set_Cursor(1,1);
-    Lcd_Write_String((char *)"5.1V");
-    __delay_ms(1000);
-    
-    while(1){
-        voltage = readADC();
-        //int charvolt = conv(voltage);
-        Lcd_Clear();
-        Lcd_Set_Cursor(1,1);
-        Lcd_Write_Int(charvolt);
-        if RA1 == 1{
-            
+    Lcd_Write_String((char *)"Range: 0");
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String((char *)".6V-5.1V");
+    __delay_ms(1500);
+
+    while(1)
+    {
+        int voltage = readADC();
+        voltage=voltage*44;
+        //itoa(voltage,voltagestring,10);
+        if (voltage>voltagemax){
+            voltagemax=voltage;
         }
-        __delay_ms(200);
+        if (displaymode==0){
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_String((char *)"Voltage=");
+            Lcd_Set_Cursor(2,1);
+            Lcd_Write_Int(voltage);
+            Lcd_Set_Cursor(2,6);
+            Lcd_Write_String((char *)"mv");
+        }/*
+        else if (displaymode==1){
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_Int(voltage);
+            Lcd_Set_Cursor(1,6);
+            Lcd_Write_String((char *)"mv");
+            Lcd_Set_Cursor(2,1);
+            Lcd_Write_Int(voltagemax);
+            Lcd_Set_Cursor(2,6);
+            Lcd_Write_String((char *)"mv");
+        }*/
+        if (RA0==1){
+            voltagesample=voltage;
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_String((char *)"Voltage");
+            Lcd_Set_Cursor(2,1);
+            Lcd_Write_String((char *)"Stored");
+            __delay_ms(1500);
+        }
+
+        if (RA1==1){
+            voltagehold(voltage);
+        }
+
+        if(RB4==1){
+            displaymode=~displaymode;
+        }
+        __delay_ms(100);
+
     }
 }
 
-
+int voltagehold(voltage){//toggles displaying fixed voltage
+    __delay_ms(200);
+    while(RA1!=1)//looks for hold button pressed a second time
+    {
+        Lcd_Set_Cursor(2,1);
+        Lcd_Write_Int(voltage);
+        Lcd_Set_Cursor(2,6);
+        Lcd_Write_String((char *)"mv");
+    }
+    return;
+}
